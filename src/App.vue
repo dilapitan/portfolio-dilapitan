f6c5e0eb1da751ef03f044182baff603ddefa818
dilapitan <domlapitan@gmail.com>
2022-06-18 15:29:37 +0800

    Update logic of hiding buttons

--- src/App.vue
+++ src/App.vue
@@ -88,24 +88,26 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn text href="https://dilapitan.dev/" target="_blank">
-        <span class="text-none"> Profile </span>
-      </v-btn>
-      <v-btn text to="/">
-        <span class="text-none"> Projects </span>
-      </v-btn>
-      <v-btn text to="/study">
-        <span class="text-none"> Study </span>
-      </v-btn>
-      <v-btn text to="/tags">
-        <span class="text-none"> Tags </span>
-      </v-btn>
-      <v-btn text href="https://codepen.io/dilapitan" target="_blank">
-        <span class="text-none"> Codepen </span>
-      </v-btn>
-      <v-btn text href="https://github.com/dilapitan" target="_blank">
-        <span class="text-none"> Github </span>
-      </v-btn>
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn text href="https://dilapitan.dev/" target="_blank">
+          <span class="text-none"> Profile </span>
+        </v-btn>
+        <v-btn text to="/">
+          <span class="text-none"> Projects </span>
+        </v-btn>
+        <v-btn text to="/study">
+          <span class="text-none"> Study </span>
+        </v-btn>
+        <v-btn text to="/tags">
+          <span class="text-none"> Tags </span>
+        </v-btn>
+        <v-btn text href="https://codepen.io/dilapitan" target="_blank">
+          <span class="text-none"> Codepen </span>
+        </v-btn>
+        <v-btn text href="https://github.com/dilapitan" target="_blank">
+          <span class="text-none"> Github </span>
+        </v-btn>
+      </template>
     </v-app-bar>
 
     <v-card color="background" flat height="100%">
